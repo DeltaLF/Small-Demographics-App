c5a9728b2487c6ceb1120fd1b67ed5afec7bcfc3
Patrick <patricktsaikl@gmail.com>
2023-04-09 21:19:30 +0800

    migrate from bootstrap to antd

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+
 import './Navbar.scss';
 
 function NavbarComponent() {
   return (
-    <Navbar expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">Logo</Navbar.Brand>
-        <Nav className="ml-auto">
-          <Nav.Item>Gear Icon</Nav.Item>
-        </Nav>
-      </Container>
-    </Navbar>
+    <div className="navbar">
+      <div className="logo">LOGO</div>
+      <div className="gear">gear icon</div>
+    </div>
   );
 }
 
