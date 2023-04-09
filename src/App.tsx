ff7e5484368302626c820cdabc88748c0d8c7a97
Patrick <patricktsaikl@gmail.com>
2023-04-09 22:39:35 +0800

    responsive home page search group layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import NavbarComponent from './components/Navbar/Navbar';
 import Home from './pages/home/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import TWWaterMark from './components/WaterMark/TWWaterMark';
 import Chart from './pages/chart/Chat';
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Layout, Select, Space } from 'antd';
+import './Home.scss';
 
 function Home() {
-  return <div>home</div>;
+  const [year, setYear] = useState<string[]>(['110']);
+  return (
+    <Layout className="Home">
+      <h1>人口數、戶數按戶別及性別統計</h1>
+      <Space className="input-group">
+        <Select
+          className="year-selector"
+          onChange={(value) => {
+            if (value.length > 0) {
+              setYear([value[1]]);
+            }
+          }}
+          mode="tags"
+          value={year}
+          options={[
+            {
+              label: '110',
+              value: '110',
+            },
+            {
+              label: '111',
+              value: '111',
+            },
+            {
+              label: '112',
+              value: '112',
+            },
+          ]}
+        />
+        <Select className="city-selector" placeholder="請選擇 縣/市">
+          縣/市
+        </Select>
+        <Select className="district-selector" placeholder="請先選擇 縣/市">
+          區
+        </Select>
+
+        <Button>submit</Button>
+      </Space>
+    </Layout>
+  );
 }
 
 export default Home;
