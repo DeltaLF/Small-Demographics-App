6b5e7757529f1dcf4fb7ddb854fbf97c1dba4e84
Patrick <patricktsaikl@gmail.com>
2023-04-10 10:32:33 +0800

    update home input group as controled elements

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import { Button, Layout, Select, Space } from 'antd';
 import './Home.scss';
+import { DISTRICTS_OPTIONS, CITY_OPTIONS } from '../../utils/city';
 
 function Home() {
-  const [year, setYear] = useState<string[]>(['110']);
+  const [year, setYear] = useState<string>('110');
+  const [city, setCity] = useState<string | undefined>();
+  const [districtsList, setDistrictsList] = useState<typeof CITY_OPTIONS>([]);
+  const [district, setDistrict] = useState<string | undefined>();
   return (
     <Layout className="Home">
       <h1>人口數、戶數按戶別及性別統計</h1>
@@ -11,35 +15,69 @@ function Home() {
         <Select
           className="year-selector"
           onChange={(value) => {
-            if (value.length > 0) {
-              setYear([value[1]]);
-            }
+            setYear(value);
+            setCity(undefined);
+            setDistrict(undefined);
           }}
-          mode="tags"
+          showSearch
           value={year}
           options={[
+            {
+              label: '106',
+              value: '106',
+            },
+            {
+              label: '107',
+              value: '107',
+            },
+
+            {
+              label: '108',
+              value: '108',
+            },
+            {
+              label: '109',
+              value: '109',
+            },
             {
               label: '110',
               value: '110',
             },
-            {
-              label: '111',
-              value: '111',
-            },
-            {
-              label: '112',
-              value: '112',
-            },
           ]}
         />
-        <Select className="city-selector" placeholder="請選擇 縣/市">
+        <Select
+          className="city-selector"
+          placeholder="請選擇 縣/市"
+          options={CITY_OPTIONS}
+          onChange={(value) => {
+            setCity(value);
+            if (value in DISTRICTS_OPTIONS) {
+              setDistrictsList(
+                DISTRICTS_OPTIONS[value as keyof typeof DISTRICTS_OPTIONS]
+              );
+            }
+            setDistrict(undefined);
+          }}
+          showSearch
+          value={city}
+        >
           縣/市
         </Select>
-        <Select className="district-selector" placeholder="請先選擇 縣/市">
+        <Select
+          className="district-selector"
+          placeholder="請先選擇 縣/市"
+          disabled={typeof city !== 'string'}
+          options={districtsList}
+          showSearch
+          value={district}
+          onChange={(value) => {
+            setDistrict(value);
+          }}
+        >
           區
         </Select>
 
-        <Button>submit</Button>
+        <Button disabled={!city || !district}>submit</Button>
       </Space>
     </Layout>
   );
